57c56d2441a132e84ceb652911122c03ade44706
Nathanael White <narhanaelw99@gmail.com>
2021-05-09 18:22:43 -0500

    created html page using only javascript and refractored my code

--- app.js
+++ app.js
@@ -1,37 +1,13 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
+
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
 const [name, github] = profileDataArgs;
 
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
 
-const generatePage = (username, githubName) => {
-    return `
-    <!DOCTYPE html>
-    <html lang="eng">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Portfolio Demo</title>
-    </head>
-
-    <body>
-        <h1>${name}</h1>
-        <h2><a href="https://github.com/${github}">Github</a></h2>
-    </body>
-    </html>
-    `;
-};
-console.log(name, github);
-console.log(generatePage(name, github));
-// const printProfileData = profileDataArr => {
-//     // this ...
-//     for (let i = 0; i < profileDataArr.length; i += 1) {
-//         console.log(profileDataArr[i]);
-//     }
-
-//     console.log('===============');
-
-//     // is the same as this
-//     profileDataArr.forEach((profileItem) => console.log(profileItem));
-// };
-
-// printProfileData(profileDataArgs);
+    console.log("Portfolio complete! Check out index.html to see the output!");
+});
